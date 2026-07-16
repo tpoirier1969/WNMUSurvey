cbebfa13230f40c992bbb1a3b88b9c329652bc78
Tod <tpoirier@nmu.edu>
2026-07-16 10:19:10 -0400

    Update interface build to 4.3.0 test

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 
   window.WNMU_CONFIG = Object.freeze({
     schemaVersion: "wnmu-viewer-questionnaire-v4",
-    buildVersion: "4.2.0-test",
+    buildVersion: "4.3.0-test",
     releaseDate: "2026-07-16",
     mode: "test",
     modeLabel: "Test Mode",
