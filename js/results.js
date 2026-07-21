d4dd6706d97800267afbfc01b0e61a925704e052
Tod <tpoirier@nmu.edu>
2026-07-21 11:41:54 -0400

    Load follow-up results modules

--- js/results.js
+++ js/results.js
@@ -1,14 +1,15 @@
 (function () {
   "use strict";
-  const version = encodeURIComponent(window.WNMU_CONFIG?.buildVersion || "");
   [
     "js/results-controller.js",
     "js/results-summary-render.js",
     "js/results-gap-render.js",
-    "js/results-export.js",
     "js/results-detail-render.js",
+    "js/results-followup-render.js",
+    "js/results-export.js",
     "js/results-copy.js",
-    "js/results-data.js"
-  ].forEach((src) => document.write(`<script src="${src}${version ? `?v=${version}` : ""}"><\/script>`));
+    "js/results-data.js",
+    "js/results-followup-data.js"
+  ].forEach((src) => document.write(`<script src="${src}?v=6.2.0-test"><\/script>`));
   document.addEventListener("DOMContentLoaded", () => init());
 })();
